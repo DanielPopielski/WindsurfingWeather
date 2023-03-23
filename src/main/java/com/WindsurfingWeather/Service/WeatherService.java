b27af53692f924b7939cdb36fa1b0d50d27624eb
DanielPopielski <danielek6699@wp.pl>
2023-03-23 19:13:54 +0100

    added WeatherService and test

--- src/main/java/com/WindsurfingWeather/Service/WeatherService.java
+++ src/main/java/com/WindsurfingWeather/Service/WeatherService.java
@@ -1,9 +1,24 @@
 package com.WindsurfingWeather.Service;
 
+import com.WindsurfingWeather.Dto.CityWeatherDto;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
+
 @Service
+@RequiredArgsConstructor
+@Data
 public class WeatherService {
 
+    @Autowired
+    private WeatherApiService weatherApiService;
 
+    public CityWeatherDto getBestCity() {
+        return weatherApiService.getCityWeatherDtoList().stream()
+                .max(Comparator.comparing(cityWeatherDto -> cityWeatherDto.getWindSpeed() * 3 + cityWeatherDto.getTemperature()))
+                .orElse(null);
+    }
 }
